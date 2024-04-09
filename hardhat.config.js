028dbf26726efb54bf4ba6390299430f67fe322b
Anton Naumtsev <naumtsevanton@mail.ru>
2024-04-09 22:21:25 +0200

    almost done

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,27 @@
-/** @type import('hardhat/config').HardhatUserConfig */
+/**
+* @type import('hardhat/config').HardhatUserConfig
+*/
+
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+
+const { API_URL, PRIVATE_KEY } = process.env;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
+
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.7.3",
+  defaultNetwork: "sepolia",
+  networks: {
+      hardhat: {},
+      sepolia: {
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
+      }
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: ETHERSCAN_API_KEY
+  }
 };
